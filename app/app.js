ed96cd13665accf9ef13e16afb97704d6cdde5c8
ranxero <javaphpsql@gmail.com>
2015-11-26 11:24:28 +0100

    json service added

--- app/app.js
+++ app/app.js
@@ -1,17 +1,8 @@
 'use strict';
 
 angular.module('angularCs', [])
-/*
 	.controller('ArticlesCtrl', function($scope, $http){
-	    $scope.articles = [
-                       { id: 1, name: "Pizza Vegetaria", price: 5 },
-                       { id: 2, name: "Pizza Salami",    price: 5.5 },
-                       { id: 3, name: "Pizza Thunfisch", price: 6 }
-                     ];
-                   });
-*/
-	.controller('ArticlesCtrl', function($scope, $http){
-	    $http.get('articles.json').then(function(articlesResponse) {
+	    $http.get('data/articles.json').then(function(articlesResponse) {
 	      $scope.articles = articlesResponse.data;
 	    });
 	  });
